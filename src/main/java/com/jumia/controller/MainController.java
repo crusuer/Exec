255b1abf616860a457d39dc197c0e4e604f14e45
Robson Pereira Silva <robsonpereira@ext.cvccorp.com.br>
2019-05-27 19:01:28 -0300

    Including index

--- src/main/java/com/jumia/controller/MainController.java
+++ src/main/java/com/jumia/controller/MainController.java
@@ -4,17 +4,24 @@ import com.jumia.entity.Customer;
 import com.jumia.service.CustomerServiceImpl;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
-@RestController
+@Controller
 public class MainController {
 
   @Autowired
   CustomerServiceImpl customerService;
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("customers", customerService.readAll());
+    return "index";
+  }
 
   @GetMapping(value = "readAll")
   public Iterable<Customer> readAll() {
